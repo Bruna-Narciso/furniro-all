0cb3bc93a11a0303083aebb3467d2572d625ad7a
brunanarciso <brunahnarcizo@gmail.com>
2026-08-26 23:01:43 -0300

    #08 feat: add shopping cart sidebar

--- furniro-desafio3/frontend/src/components/Header/RigthMenu.tsx
+++ furniro-desafio3/frontend/src/components/Header/RigthMenu.tsx
@@ -2,17 +2,17 @@ import clsx from "clsx";
 import { Link } from "react-router";
 import { useState } from "react";
 
-import { useCartStore } from "../../stores/cart.store";
 import LogoutButton from "../Login/LogoutButton";
+import CartButton from "./CartButton";
+import ShoppingCart from "../Cart/ShoppingCart";
 
 type RightMenuProps = {
   className?: string;
 };
 
 const RightMenu = ({ className }: RightMenuProps) => {
-  const totalItems = useCartStore((s) => s.getTotalItems());
-
   const [showLogout, setShowLogout] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const savedUser = localStorage.getItem("user");
   const user = savedUser ? JSON.parse(savedUser) : null;
@@ -21,81 +21,66 @@ const RightMenu = ({ className }: RightMenuProps) => {
     "hover:cursor-pointer hover:scale-110 transition";
 
   return (
-    <div
-      className={clsx(
-        "flex items-center gap-[33.66px]",
-        className,
-      )}
-    >
+    <>
+      <div
+        className={clsx(
+          "flex items-center gap-[33.66px]",
+          className,
+        )}
+      >
+        {/* USUÁRIO */}
+        {user ? (
+          <div className="relative">
+            <button
+              type="button"
+              onClick={() =>
+                setShowLogout((previous) => !previous)
+              }
+              className={clsx(
+                "flex items-center gap-2",
+                LinkHover,
+              )}
+            >
+              <img
+                src="/Icons/alert.svg"
+                alt="Ícone de usuário"
+                className="max-h-[22px]"
+              />
 
-      {/* USUÁRIO */}
-      {user ? (
-        <div className="relative">
-          <button
-            type="button"
-            onClick={() => setShowLogout((previous) => !previous)}
-            className={clsx(
-              "flex items-center gap-2",
-              LinkHover,
+              <span>{user.firstName}</span>
+            </button>
+
+            {showLogout && (
+              <div className="absolute right-0 top-8 z-50 bg-white p-3 shadow-md">
+                <LogoutButton />
+              </div>
             )}
+          </div>
+        ) : (
+          <Link
+            to="/login"
+            className={clsx(LinkHover)}
           >
             <img
               src="/Icons/alert.svg"
-              alt="Ícone de usuário"
+              alt="Fazer login"
               className="max-h-[22px]"
             />
-
-            <span>{user.firstName}</span>
-          </button>
-
-          {showLogout && (
-            <div className="absolute right-0 top-8 z-50 bg-white p-3 shadow-md">
-              <LogoutButton />
-            </div>
-          )}
-        </div>
-      ) : (
-        <Link
-          to="/login"
-          className={clsx(LinkHover)}
-        >
-          <img
-            src="/Icons/alert.svg"
-            alt="Fazer login"
-            className="max-h-[22px]"
-          />
-        </Link>
-      )}
-
-      {/* CARRINHO */}
-      <Link
-        to="/cart"
-        className={clsx(
-          LinkHover,
-          "relative",
+          </Link>
         )}
-      >
-        <img
-          src="/Icons/shop.svg"
-          alt="Ícone do carrinho"
-          className="max-h-[22.05px]"
+
+        {/* CARRINHO */}
+        <CartButton
+          onClick={() => setIsCartOpen(true)}
         />
+      </div>
 
-        {totalItems > 0 && (
-          <span
-            className={clsx(
-              "absolute -right-3 -top-3",
-              "flex h-4.5 w-4.5 items-center justify-center",
-              "rounded-full",
-              "bg-over-secundary",
-              "text-xs font-bold text-white",
-            )}
-          >
-            {totalItems}
-          </span>
-        )}
-      </Link>
-    </div>
+      {/* SIDEBAR DO CARRINHO */}
+      <ShoppingCart
+        isOpen={isCartOpen}
+        onClose={() => setIsCartOpen(false)}
+      />
+    </>
   );
 };
 
